78d94a53fde68326b452731d6b55b99ee6c95e58
Klyta <55992278+Klyta@users.noreply.github.com>
2019-12-17 14:24:55 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,4 @@
-#include "Player.h"
+include "Player.h"
 #include "fstream"
 #include "CavalryFactory.h"
 #include "Cell.h"
@@ -28,19 +28,13 @@ int FromStrToNumber(std::string str)// string
 	}
 	return number;
 }
-void Player::SaveGame() const
+void Player::SaveGame() 
 {
 	if (playerObj.empty())
 	{
 		std::cout << "Eror,steck is empty" << std::endl;
 	}
 	std::string str;
-      str = playerObj.top()->toString();
-	std::cout << "Save Game -" <<  std::endl;
-//for (int i = 0; i < playerObj.size(); i++)
-//{
-//	std::cout << "Save Game - " << playerObj.Peek(i) ->toString() << std::endl;
-//}
 	std::ofstream ou;
 	ou.open("Output.txt");
 	if (ou.is_open())
@@ -51,8 +45,13 @@ void Player::SaveGame() const
 	{
 		std::cout << "Something wrong!" << std::endl;
 	}
-
-	ou << str;
+	for (int i = 0; i < playerObj.size(); i++)
+	{
+		str = playerObj.top()->toString();
+	 ou << str <<" ";
+	//	std::cout << "Save Game -" << std::endl;
+		playerObj.pop();
+	}
 	std::cout << "Save - " << str << std::endl;
 	ou.close();
 }
@@ -77,18 +76,108 @@ void Player::Load()
 		std::cout << "Something wrong!" << std::endl;
 	}
 	std::string str;
-	ou >> str;
+	//ou >> str;
+	getline(ou, str);
 	int i = 0;
 	{
-		if (str[i] == 'C')
+		if (str[i] == 'U' && str[i + 1] == 'C')       //C
 		{
 			std::cout << "Cav";
+			int playerNum = 0;
+			int Damege = 0;
+			int Health = 0;
+			int x = 0;
+			int y = 0;
+			std::string T;
+			while (str[i] != ';')
+			{
+				playerNum = FromStrToNumber(&str[i], 1);
+				i++;
+			}
+			if (playerNum == 0)
+			{
+				std::cout << "Eror in load(p.n.)";
+			}
+			else
+			{
+				std::cout << playerNum;
+			}
+			while (str[i] != 'H')
+			{
+				i++;
+			}
+			std::string strx;
+			i++;
+			while (str[i] != ';')
+			{
+
+				strx += std::to_string(FromStrToNumber(&str[i], 1));
+				i++;
+			}
+			Health = FromStrToNumber(strx) ;
+			std::cout << "H = " << Health ;
+			while (str[i] != 'D')
+			{
+				i++;
+			}
+			std::string stry;
+			i++;
+			while (str[i] != ';')
+			{
+
+				stry += std::to_string(FromStrToNumber(&str[i], 1));
+				i++;
+			}
+			Damege = FromStrToNumber(stry);
+			std::cout << "D = " << Damege ;
+			while (str[i] != ':')
+			{
+				i++;
+			}
+			while (str[i] != ';')
+			{
+				//strx = std::to_string(str[i]);
+				x = FromStrToNumber(&str[i], 1);
+				i++;
+			}
+			//x = FromStrToNumber(strx);
+			std::cout << "X = " << x << std::endl;
+
+			while (str[i] != ':')
+			{
+				i++;
+			}
+			while (str[i] != ';')
+			{
+
+				y = FromStrToNumber(&str[i], 1);
+				i++;
+			}
+
+			std::cout << "Y = " << y << std::endl;
+
+			while (str[i] != ':')
+			{
+				i++;
+			}
+			while (str[i + 1] != '/')
+			{
+
+				T += str[i + 1];
+				i++;
+			}
+
+			std::cout << "T = " << T << std::endl;
+			Cell cell(T, y, x);
+			Cavalry* c = new Cavalry(Health,Damege,&cell, this);
+			std::cout << "Unit c" << std::endl;
+			std::cout << "H-  " << c->getHealth() << "D-  " << c->getDemage() << "P-  " << c->getPlayer().GetPlayerNumber() << c->getUnitCell()->getOllData() << std::endl;
 		}
-		if (str[i] == 'B')
+		if (str[i]=='U' && str[i+1] == 'B')       //B
 		{
 			std::cout << "Bild";
 		}
-		if (str[i] == 'F' && str[i + 1] == 'C')
+		if (str[i] == 'F' && str[i + 1] == 'C')        //FC
 		{
 			std::cout << "CavFac";
 
@@ -123,7 +212,7 @@ void Player::Load()
 			//        while (str[i] != ';')
 			//		{
 			//			x = FromStrToNumber(&str[i], 1);
-//std::cout << "Strx = " << strx;
+			//std::cout << "Strx = " << strx;
 			//			i++;
 			//		}
 			while (str[i] != ':')
@@ -138,43 +227,42 @@ void Player::Load()
 			}
 			//x = FromStrToNumber(strx);
 			std::cout << "X = " << x << std::endl;
-		
-		while (str[i] != ':')
-		{
-			i++;
+
+			while (str[i] != ':')
+			{
+				i++;
+			}
+			while (str[i] != ';')
+			{
+
+				y = FromStrToNumber(&str[i], 1);
+				i++;
+			}
+
+			std::cout << "Y = " << y << std::endl;
+
+			while (str[i] != ':')
+			{
+				i++;
+			}
+			while (str[i + 1] != '/')
+			{
+
+				T += str[i + 1];
+				i++;
+			}
+
+			std::cout << "T = " << T << std::endl;
+			Cell cell(T, y, x);
+			CavalryFactory* cFactoryPtr = new CavalryFactory(cell, this);
+			std::cout << "Factory F" << std::endl;
 		}
-		while (str[i] != ';')
-		{
-			
-			y = FromStrToNumber(&str[i], 1);
-			i++;
-		}
-		
-		std::cout << "Y = " << y << std::endl;
-	
-		while (str[i] != ':')
-		{
-			i++;
-		}
-		while (str[i+1] != '/')
-		{
-			
-			T += str[i+1];
-			i++;
-		}
-	
-		std::cout << "T = " << T << std::endl;
-		Cell cell(T, y, x);
-		CavalryFactory* cFactoryPtr = new CavalryFactory(cell,this);
-		std::cout << "Factory F" << std::endl;
-	}
 		else
 		{
 			std::cout << "Eror in load" << std::endl;
 		}
 		std::cout << std::endl;
-		
-    }
-	std::cout << "Load - " << str << std::endl;
-	ou.close();
+		std::cout << "Load - " << str << std::endl;
+		ou.close();
+	}
 }
